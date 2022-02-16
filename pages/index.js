675a77547a05aafd142d0c1f48f01056a7ff170b
Martini <me.martiinii@gmail.com>
2022-02-16 14:58:38 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = () => {
       <title>Strona główna</title>
     </Head>
 
-    <div className="flex w-full justify-center p-10">
-      <h3 className="text-red-500 text-xl font-medium">Witamy w NEXT.js</h3>
+    <div className="flex w-full h-screen justify-center items-center p-10">
+      <h3 className="text-sky-600 text-xl font-medium p-10 bg-slate-900 rounded-3xl">NAPIS</h3>
     </div>
     </>
   )
