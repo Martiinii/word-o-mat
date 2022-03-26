ca4a77369b2feae09d934af1cf8af21301e3e161
Martini <me.martiinii@gmail.com>
2022-03-26 21:27:16 +0100

    Added 'noPadding' setting

--- components/ContainerMotion.js
+++ components/ContainerMotion.js
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
-const ContainerMotion = ({ children, className, ...props }) => {
+const ContainerMotion = ({ children, noPadding, className, ...props }) => {
     return (
         <motion.div
             layout
-            className={`bg-white m-2 p-3 md:m-5 md:p-5 rounded-xl md:rounded-3xl shadow-black/50 shadow-lg w-fit font-medium ${className}`}
+            className={`bg-white m-2 md:m-5 ${noPadding || 'p-3 md:p-5'} rounded-xl md:rounded-3xl shadow-black/50 shadow-lg w-fit font-medium ${className}`}
             initial={{scale: 1, opacity: 0}}
             animate={{opacity: 1}}
             exit={{scale: .5, opacity: 0}}
