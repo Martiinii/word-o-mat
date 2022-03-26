b4aa92436e719374f5f7eeaf85ad8da933e1a4d0
Martini <me.martiinii@gmail.com>
2022-03-26 21:27:39 +0100

    Link was broken

--- components/ListComponents/AddCard.js
+++ components/ListComponents/AddCard.js
@@ -5,12 +5,12 @@ import ContainerMotion from "../ContainerMotion";
 
 const AddCard = () => {
     return (
-        <ContainerMotion>
-        <Link href={`/edit/new`} passHref>
-            <a className="group text-6xl">
-                <FontAwesomeIcon className="m-5 text-sky-600 group-hover:text-sky-700 transition-all" icon={faPlus} />
-            </a>
-        </Link>
+        <ContainerMotion noPadding>
+            <Link href={`/edit/new`} passHref>
+                <a className="group block h-full p-3 md:p-5 text-6xl">
+                    <FontAwesomeIcon className="m-5 text-sky-600 group-hover:text-sky-700 transition-all" icon={faPlus} />
+                </a>
+            </Link>
         </ContainerMotion>
     );
 }
