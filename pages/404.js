b3acafd2547779875705970f9b7ffb2e678474a7
Martini <me.martiinii@gmail.com>
2022-03-27 14:42:30 +0200

    Added margin

--- pages/404.js
+++ pages/404.js
@@ -9,10 +9,11 @@ const Custom404 = () => {
             </Head>
 
             <Container className="flex flex-col gap-5 text-center">
-                <h1 className="font-bold text-8xl">404</h1>
+                <h1 className="font-bold text-8xl m-3">404</h1>
                 <p className="text-stone-700 text-lg">This page could not be found.</p>
             </Container>
         </>
     )
 }
+
 export default Custom404;
