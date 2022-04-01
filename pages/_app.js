65298fd190307ffaa2ce6cc60c76748babde805f
Martini <me.martiinii@gmail.com>
2022-04-01 20:45:06 +0200

    Moved font to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Template from "../components/Template"
 import "../styles/globals.css"
 
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
@@ -11,7 +12,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Template>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
         <link rel="shortcut icon" href="/images/logo.svg" />
+
       </Head>
 
       <Component {...pageProps} />
