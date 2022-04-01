a04fb1ff9917e71f5cd63b92a553cbd5f4b27ef3
Martini <me.martiinii@gmail.com>
2022-04-01 20:23:22 +0200

    Moved style to css file

--- components/useInput.js
+++ components/useInput.js
@@ -5,8 +5,8 @@ const useInput = ({ placeholder, className, defaultValue, ...props }) => {
 
     const input = (
         <input
-            className={`bg-indigo-50 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:bg-indigo-100 focus:ring-offset-4 text-indigo-900 p-1 px-2 font-medium w-36 md:font-medium md:text-lg md:p-2 md:px-4 md:w-fit transition-all ${className}`}
-            placeholder={placeholder}
+            className={`in ${className ?? ""}`}
+            placeholder={placeholder ?? ""}
             value={value}
             onChange={e => setValue(e.target.value)}
             {...props}
