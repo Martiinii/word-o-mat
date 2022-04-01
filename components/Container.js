b0002ff19250d1d8967df27c45e6c1e47982ead1
Martini <me.martiinii@gmail.com>
2022-04-01 20:22:55 +0200

    Moved style to css file

--- components/Container.js
+++ components/Container.js
@@ -4,7 +4,7 @@ const Container = React.forwardRef(({ children, className, as, ...props }, ref) 
     return React.createElement(
         as,
         {
-            className: `bg-white m-2 p-3 md:m-5 md:p-5 rounded-xl md:rounded-3xl shadow-black/50 shadow-lg w-fit font-medium ${className}`,
+            className: `cont ${className}`,
             ref: ref,
             ...props
         },
--- components/ContainerMotion.js
+++ components/ContainerMotion.js
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
-const ContainerMotion = ({ children, noPadding, className, ...props }) => {
+const ContainerMotion = ({ children, className, ...props }) => {
     return (
         <motion.div
             layout
-            className={`bg-white m-2 md:m-5 ${noPadding || 'p-3 md:p-5'} rounded-xl md:rounded-3xl shadow-black/50 shadow-lg w-fit font-medium ${className}`}
+            className={`cont ${className}`}
             initial={{scale: 1, opacity: 0}}
             animate={{opacity: 1}}
             exit={{scale: .5, opacity: 0}}
