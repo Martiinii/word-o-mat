7d48a2cfff55db85c8f958f9f0ea342bb8e2f854
Martini <me.martiinii@gmail.com>
2022-04-07 19:02:02 +0200

    Added button to flip words

--- components/NewList/NewListContainer.js
+++ components/NewList/NewListContainer.js
@@ -3,6 +3,9 @@ import Table from "./Table";
 import useInput from '../useInput';
 import { useEffect } from "react";
 import { motion } from "framer-motion";
+import ButtonComponent from "../ButtonComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
 const NewListContainer = ({ ready, title, updateTitle, words, updateWords }) => {
     const [titleInputValue, titleInput, setTitleInput] = useInput({ placeholder: 'Title', className: 'text-center' })
@@ -15,13 +18,28 @@ const NewListContainer = ({ ready, title, updateTitle, words, updateWords }) => 
         updateTitle(titleInputValue);
     }, [titleInputValue]);
 
+    const flipWords = () => {
+        let temp = { ...words };
+        temp.ids.forEach(id => {
+            [temp[id].orig, temp[id].trans] = [temp[id].trans, temp[id].orig];
+        });
+
+        updateWords(temp);
+    }
+
 
     return (
         <ContainerMotion className="max-w-full text-center bg-white p-5">
-            <motion.div layout>
+            <motion.div layout className="flex gap-5 justify-center items-center">
+                <div className="flex-1" />
                 {titleInput}
+                <div className="flex-1 text-left">
+                    <ButtonComponent className="text-xl md:text-3xl bg-sky-400 hover:bg-sky-500 focus:ring-sky-400" onClick={flipWords}>
+                        <FontAwesomeIcon icon={faRotate} className="opacity-70" />
+                    </ButtonComponent>
+                </div>
             </motion.div>
-                <Table words={words} setWords={updateWords} />
+            <Table words={words} setWords={updateWords} />
         </ContainerMotion>
 
     )
